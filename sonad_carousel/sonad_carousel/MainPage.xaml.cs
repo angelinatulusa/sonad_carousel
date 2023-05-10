6c742b444a5750816d24fc207209bc2bbd98ae32
Zara <Zara@DESKTOP-R22G3DK>
2023-05-11 00:21:25 +0300

    Добавьте файлы проекта.

---
+++ sonad_carousel/sonad_carousel/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace sonad_carousel
+{
+    public partial class MainPage : ContentPage
+    {
+        Button loomad_btn, puuviljad_btn;
+        public MainPage()
+        {
+            loomad_btn = new Button
+            {
+                Text = "Loomad",
+            };
+            puuviljad_btn = new Button
+            {
+                Text = "Puuviljad"
+            };
+            loomad_btn.Clicked += Loomad_Clicked;
+            puuviljad_btn.Clicked += Puuviljad_Clicked;
+            StackLayout st = new StackLayout
+            {
+                Orientation = StackOrientation.Vertical,
+                BackgroundColor = Color.Beige,
+                Children = { loomad_btn, puuviljad_btn }
+            };
+            Content = st;
+        }
+
+        private async void Puuviljad_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new puuviljad());
+        }
+
+        private async void Loomad_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new loomad());
+        }
+    }
+}
---
+++ sonad_carousel/sonad_carousel/loomad.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace sonad_carousel
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class loomad : CarouselPage
+    {
+        public loomad()
+        {
+            var redContentPage = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    Children =
+                    {
+                        new Label
+                        {
+                            Text = "Red",
+                            FontSize = Device.GetNamedSize (NamedSize.Medium, typeof(Label)),
+                            HorizontalOptions = LayoutOptions.Center,
+                        },
+                        new BoxView
+                        {
+                            Color= Color.Red,
+                            WidthRequest= 200,
+                            HeightRequest= 200,
+                            HorizontalOptions= LayoutOptions.Center,
+                            VerticalOptions= LayoutOptions.CenterAndExpand,
+                        }
+                    }
+                }
+            };
+            Children.Add(redContentPage);
+        }
+    }
+}
