5824f0d230492fcc716ef0abd4cf12cd921ad84b
opilane <opilane@HP-CZC2349HTM>
2023-05-23 14:55:08 +0300

    asd

--- sonad_carousel/sonad_carousel/MainPage.xaml.cs
+++ sonad_carousel/sonad_carousel/MainPage.xaml.cs
@@ -3,43 +3,70 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace sonad_carousel
 {
     public partial class MainPage : ContentPage
     {
-        Button loomad_btn, puuviljad_btn;
+        string text = "Kass:Cat;" + "Koer:Dog;";
+        Button corousel, uusloom;
+        Editor eloom, vloom;
+        Label uus;
+        string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Loomad.txt");
         public MainPage()
         {
-            loomad_btn = new Button
+            File.WriteAllText(path, text);
+            StackLayout stack = new StackLayout();
+            corousel = new Button()
             {
-                Text = "Loomad",
+                Text = "Loomad carousel",
+                BackgroundColor = Color.Wheat,
+                CornerRadius= 10,
             };
-            puuviljad_btn = new Button
+            uus = new Label()
             {
-                Text = "Puuviljad"
+                Text = "Kirjuta loomi nimi eesti ja inglise keeles",
+                BackgroundColor = Color.Wheat,
             };
-            loomad_btn.Clicked += Loomad_Clicked;
-            puuviljad_btn.Clicked += Puuviljad_Clicked;
-            StackLayout st = new StackLayout
+            eloom = new Editor()
             {
-                Orientation = StackOrientation.Vertical,
-                BackgroundColor = Color.Beige,
-                Children = { loomad_btn, puuviljad_btn }
+                Placeholder = "Loom eesti keeles"
             };
-            Content = st;
+            vloom = new Editor()
+            {
+                Placeholder = "Loom inglise keeles"
+            };
+            uusloom = new Button()
+            {
+                Text = "Lisa uus loom",
+                BackgroundColor = Color.Wheat,
+                CornerRadius = 10,
+            };
+            corousel.Clicked += Corousel_Clicked;
+            stack.Children.Add(corousel);
+            stack.Children.Add(uus);
+            stack.Children.Add(eloom);
+            stack.Children.Add(vloom);
+            stack.Children.Add(uusloom);
+            uusloom.Clicked += Uus_kart_Clicked;
+            Content = stack;
+        }
+        string uus_tekst;
+        private void Uus_kart_Clicked(object sender, EventArgs e)
+        {
+            uus_tekst = eloom.Text + ":" + vloom.Text + ";";
+            File.AppendAllText(path, uus_tekst);
+            eloom.Text = null;
+            vloom.Text = null;
         }
 
-        private async void Puuviljad_Clicked(object sender, EventArgs e)
+        private async void Corousel_Clicked(object sender, EventArgs e)
         {
-            await Navigation.PushAsync(new puuviljad());
-        }
-
-        private async void Loomad_Clicked(object sender, EventArgs e)
-        {
-            await Navigation.PushAsync(new loomad());
+            await Navigation.PushAsync(new NavigationPage(new loomad(path)));
         }
     }
 }
--- sonad_carousel/sonad_carousel/loomad.xaml.cs
+++ sonad_carousel/sonad_carousel/loomad.xaml.cs
@@ -2,42 +2,72 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-
+using System.IO;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+using System.Runtime.CompilerServices;
+using Xamarin.Essentials;
+using System.Runtime.InteropServices.ComTypes;
 
 namespace sonad_carousel
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class loomad : CarouselPage
     {
-        public loomad()
+        string[] dir;
+        string[] sonad;
+        public loomad(string path)
         {
-            var redContentPage = new ContentPage
+            string dir_FilePath = Path.Combine(path, "Loomad.txt");
+            string dir_Text = File.ReadAllText(path);
+            dir = dir_Text.Split(';');
+            int length = dir.Length;
+            for (int i = 0; i < length - 1; i++)
             {
-                Content = new StackLayout
+                string rnd = dir[i];
+                sonad = rnd.Split(':');
+                var Page = new ContentPage
                 {
-                    Children =
+                    Content = new StackLayout
                     {
-                        new Label
+                        TabIndex = i,
+                        Children =
                         {
-                            Text = "Red",
-                            FontSize = Device.GetNamedSize (NamedSize.Medium, typeof(Label)),
-                            HorizontalOptions = LayoutOptions.Center,
-                        },
-                        new BoxView
-                        {
-                            Color= Color.Red,
-                            WidthRequest= 200,
-                            HeightRequest= 200,
-                            HorizontalOptions= LayoutOptions.Center,
-                            VerticalOptions= LayoutOptions.CenterAndExpand,
+                            new Label
+                            {
+                                FontSize= 20,
+                                Text = sonad[0],
+                                BackgroundColor = Color.Wheat,
+                                TabIndex= i
+                            }
                         }
                     }
-                }
-            };
-            Children.Add(redContentPage);
+                };
+                Children.Add(Page);
+                var lbl = ((StackLayout)Page.Content).Children[0] as Label;
+                var tap = new TapGestureRecognizer();
+                lbl.GestureRecognizers.Add(tap);
+                tap.Tapped += Tap_Tapped;
+
+            }
+        }
+        bool OnOff = true;
+        private void Tap_Tapped(object sender, EventArgs e)
+        {
+            if (OnOff)
+            {
+                var lbl = sender as Label;
+                string Tekst_on = dir[lbl.TabIndex];
+                sonad = Tekst_on.Split(':');
+                lbl.Text += " - " + sonad[1];
+                OnOff = false;
+            }
+            else
+            {
+                var lbl = sender as Label;
+                string Tekst_on = dir[lbl.TabIndex];
+                sonad = Tekst_on.Split(':');
+                lbl.Text = sonad[0];
+                OnOff = true;
+            }
         }
     }
 }
